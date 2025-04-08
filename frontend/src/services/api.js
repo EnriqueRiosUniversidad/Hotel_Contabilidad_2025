16e723fd9fd0c399862dc716a898ee8d784f53ee
EnriqueRiosUniversidad <enrique.rios2019@fiuni.edu.py>
2025-04-08 13:50:40 -0400

    esta funcionando

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'https://hotel-contabilidad-35ebeef89ac8.herokuapp.com',
+  baseURL: 'https://hotel-contabilidad-35ebeef89ac8.herokuapp.com',
 
-  baseURL: 'http://localhost:8080',
+  //baseURL: 'http://localhost:8080',
 });
 
 export default api;
